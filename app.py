614e324939f23a291a61b69a6e6e95d9dcdfb97d
Kamil Kaygisiz <kamilkaygisizz@gmail.com>
2023-08-03 16:36:18 +0300

    little improvements on n11 controller

--- app.py
+++ app.py
@@ -18,27 +18,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db.init_app(app)
 
 async def start_tasks(database_uri):
-    from controller.hepsiburda_notebook_controller import hepsiburada_blueprint
-   # from scheduler.hepsiburda_notebook_scheduler import schedule_task_for_hepsiburada
-    from controller.vatan_notebook_controller import vatan_blueprint
+    from scheduler.hepsiburda_notebook_scheduler import schedule_task_for_hepsiburada
     from scheduler.vatan_notebook_scheduler import schedule_task_for_vatan
     from scheduler.n11_notebook_scheduler import schedule_task_for_n11
-    #from controller.vatan_notebook_controller import vatan_blueprint
+    from controller.n11_notebook_controller import n11_blueprint
+    from controller.vatan_notebook_controller import vatan_blueprint
+    from controller.hepsiburda_notebook_controller import hepsiburada_blueprint
+
+
 
     app.register_blueprint(hepsiburada_blueprint, url_prefix='/api/hepsiburda')
     app.register_blueprint(vatan_blueprint,url_prefix='/api/vatan')
-    #app.register_blueprint(vatan_blueprint, url_prefix='/api/vatan')
+    app.register_blueprint(n11_blueprint,url_prefix='/api/n11')
+
 
     # Get the current event loop (Flask's event loop)
     loop = asyncio.get_event_loop()
 
     # Run the scheduler for the Hepsiburada notebook in the current event loop
     n11_task=asyncio.create_task(schedule_task_for_n11(app,db,database_uri))
-   # hepsiburada_task = asyncio.create_task(schedule_task_for_hepsiburada(app,db,database_uri))  # Pass the database URI as a parameter
+    hepsiburada_task = asyncio.create_task(schedule_task_for_hepsiburada(app,db,database_uri))  # Pass the database URI as a parameter
     vatan_task = asyncio.create_task(schedule_task_for_vatan(app,db, database_uri))
     
     # Run the scheduler in the current event loop
-    #await hepsiburada_task
+    await hepsiburada_task
     await vatan_task
     await n11_task
 if __name__ == "__main__":
