5f59688a02fa444a1d6f90487c9a317c323affd1
Kamil Kaygisiz <kamilkaygisizz@gmail.com>
2023-08-12 03:03:02 +0300

    Complete project entry file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import asyncio
 import threading
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from flask import Blueprint, jsonify  # Import other necessary modules
+from flask import Blueprint, jsonify 
 from model.dbmodel import db
 from model.hepsiburda_datas import HepsiburadaData 
 from model.vatandatas import VatanData
@@ -12,14 +12,11 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)  # frontend in apilere erisebilmesi icin corsu tanimla cors hatasini engelle
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@localhost:5432/FinalProject'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-# No need to initialize the databases again
-# hepsiburda_db.init_app(app)
-# vatan_db.init_app(app)
-#db = SQLAlchemy()
+#Database'i baslat singleton baglanti modeliyle diger servicelerin ayni instance-baglantiyi kullanmasini sagla
 
 db.init_app(app)
 with app.app_context():
@@ -36,8 +33,6 @@ async def start_tasks(database_uri):
     
 
 
-
-
     app.register_blueprint(hepsiburada_blueprint, url_prefix='/api/hepsiburda')
     app.register_blueprint(vatan_blueprint,url_prefix='/api/vatan')
     app.register_blueprint(n11_blueprint,url_prefix='/api/n11')
@@ -47,36 +42,26 @@ async def start_tasks(database_uri):
     app.register_blueprint(all_screen_sizes_blueprint,url_prefix='/api/aggregated')
     app.register_blueprint(all_cheapest_computers_blueprint,url_prefix='/api/aggregated')
 
-
-    
-
-
-
-
-    # Get the current event loop (Flask's event loop)
+    # Flask event loopunu al
     loop = asyncio.get_event_loop()
-
-    # Run the scheduler for the Hepsiburada notebook in the current event loop
-    #n11_task=asyncio.create_task(schedule_task_for_n11(app,db,database_uri))
-    #hepsiburada_task = asyncio.create_task(schedule_task_for_hepsiburada(app,db,database_uri))  # Pass the database URI as a parameter
+    #Background taski ekle
+    n11_task=asyncio.create_task(schedule_task_for_n11(app,db,database_uri))
+    hepsiburada_task = asyncio.create_task(schedule_task_for_hepsiburada(app,db,database_uri))  # Database uri'ini parametre olarak gecir
     vatan_task = asyncio.create_task(schedule_task_for_vatan(app,db, database_uri))
     transform_task=asyncio.create_task(scheduler(app,db, database_uri))
 
-
+    #async await mantigi ile bir gorevin calismasinin digerini etkilememesi icin await ile gorevleri cagir
     await transform_task 
-
-    # Run the scheduler in the current event loop
-    #await hepsiburada_task
+    await hepsiburada_task
     await vatan_task
-    #await n11_task
+    await n11_task
 
 if __name__ == "__main__":
-    # Create the database tables based on the models
-   
-
+   #serveri ayri bir threadde calisir
     server_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
     server_thread.start()
     try:
+        #flask background tasklerini diger bir threadde calistir
         loop = asyncio.get_event_loop()
         loop.run_until_complete(start_tasks(app.config['SQLALCHEMY_DATABASE_URI']))
         server_thread.join()    
@@ -84,7 +69,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Interrupted.")
         print("Exiting.")
-        #create the model create the scheduler run that scheduler from main
         
         
         
